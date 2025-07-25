f68e19b158d04293fee9bf724984ead2dcb547a9
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-07-25 23:50:26 +0530

    basics

---
+++ 01_basics/input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+     int apples;
+     cout <<"Enter the No. Of Apples: ";
+     cin>>apples;
+     cout<<"Number of Apples: "<<apples<<endl;
+    return 0;
+}

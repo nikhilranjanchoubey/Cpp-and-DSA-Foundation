0d060b8fee7872fb0f9cfa52db5012751059446a
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-01 22:10:58 +0530

    basics

---
+++ 01_basics/sum.cpp
@@ -0,0 +1,12 @@
+// Take 2 integers as input and print their sum.
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "Sum: " << sum << endl;
+    return 0;
+}

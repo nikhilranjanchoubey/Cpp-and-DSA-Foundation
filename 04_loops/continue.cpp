000e4bd21ee7581b523b2c4952ae908d113f2508
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-04 00:05:23 +0530

    loop

---
+++ 04_loops/continue.cpp
@@ -0,0 +1,16 @@
+// Print all the values between 1 to 50 except for the multiple of 3.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1;i<=50;i++){
+        if(i%3==0){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+
+   return 0;
+}

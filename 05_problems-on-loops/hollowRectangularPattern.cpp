45e08079352249999c4a34da737cae495b9120e2
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-04 22:38:03 +0530

    pattern

--- 05_problems-on-loops/hollowRectangularPattern.cpp
+++ 05_problems-on-loops/hollowRectangularPattern.cpp
@@ -11,8 +11,8 @@ int main(){
 
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            if (i == 1 || i == n || j == 1 || j == m)
-            { cout<<"*";
+            if (i == 1 || i == n || j == 1 || j == m){
+             cout<<"*";
             }
            else { 
             cout<<" ";

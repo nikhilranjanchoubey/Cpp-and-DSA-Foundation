6a4eeae66f40cbd440a1dab96239f47164439d70
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-04 23:07:37 +0530

    pattern

--- 05_problems-on-loops/triangularPattern2.cpp
+++ 05_problems-on-loops/triangularPattern2.cpp
@@ -8,8 +8,8 @@ int main(){
     cin>>n;
     
 
-    for (int i=0; i<=n;i++){ 
-        for (int j=i+1;j<=n;j++){ 
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++){ 
         cout<<"*";
         }
        cout<<endl; 

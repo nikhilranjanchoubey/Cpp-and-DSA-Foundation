fd1425476ebadbe52471dcb1881fd83e742839e6
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-04 23:47:40 +0530

    pattern

--- 05_pattern_printing/numericalPattern3.cpp
+++ 05_pattern_printing/numericalPattern3.cpp
@@ -12,8 +12,11 @@ int main(){
         for (int j=1;j<=(n-i);j++){ 
         cout<<" ";
         }
-        for(int j=1; j<=(2*i-1); j++){
-            cout<<"*";
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        for(int j=(i-1); j>=1; j--){
+            cout<<j;
         }
         cout<<endl;      
     }
--- 05_pattern_printing/numericalTriangle.cpp
+++ 05_pattern_printing/numericalTriangle.cpp
@@ -9,14 +9,13 @@ int main(){
     
 
     for (int i=1; i<=n;i++){ 
-        for (int j=1;j<=(n-i);j++){ 
-        cout<<" ";
+        for (int j=1;j<=i;j++){ 
+        cout<<j;
         }
-        for(int j=1; j<=(2*i-1); j++){
-            cout<<"*";
-        }
-        cout<<endl;      
+       cout<<endl; 
     }
-    
+
+
+
     return 0;
 }

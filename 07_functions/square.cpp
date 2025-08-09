b4169afcdb720dde976708b5ac67096c22e417ff
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-09 10:16:38 +0530

    function

---
+++ 07_functions/square.cpp
@@ -0,0 +1,19 @@
+// write a function to print square of first 5 natural numbers.
+// Sample Output: 1 4 9 16 25
+
+#include<iostream>
+using namespace std;
+
+int square(int n){
+    return n * n;
+}
+
+int main(){
+
+    for (int i=1;i<=5;i++){
+        cout<<square(i);
+        cout<<endl;
+    } 
+
+    return 0;
+}

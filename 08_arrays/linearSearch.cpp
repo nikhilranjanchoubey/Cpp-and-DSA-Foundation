ce4b09f637afe9a16cab9e02bb6226bc4f10de49
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-10 14:35:18 +0530

    arrays

--- 08_arrays/linearSearch.cpp
+++ 08_arrays/linearSearch.cpp
@@ -1,12 +1,40 @@
 // Search if a given element is present in the array or not.
-// If it is not present then return 1 else return the index.
+// If it is not present then return -1 else return the index.
 #include<iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    // int arr[] = {3, 9, 18, 11, 7};
+    // int find = 11;
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    // int index = -1;
 
+    // for (int i = 0; i < size; i++) {
+    //     if (arr[i] == find) {
+    //         index = i;
+    //         break;
+    //     }
+    // }
 
+    // if (index != -1) {
+    //     cout << index;
+    // } else {
+    //     cout << "1";
+    // }
+
+    int array[] = {3, 9, 18, 11, 7};
+    int key = 11;
+
+    int ans = -1;
+
+    for (int i = 0; i < 5; i++){
+        if(array[i]==key){
+            ans=i;
+            break;
+        }
+    }
+
+    cout<<ans<<endl;
 
     return 0;
 }

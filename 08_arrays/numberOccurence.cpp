98dc964785402488771d6161c9164d2e477b1f24
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-08-12 22:38:46 +0530

    vectors

---
+++ 08_arrays/numberOccurence.cpp
@@ -0,0 +1,27 @@
+// Count the number of occurences of a particular element X.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+
+    vector<int> v(6);
+    for(int i=0; i<v.size(); i++){
+        cin>>v[i];
+    }
+
+    cout<<"Enter x: ";
+    int x;
+    cin>>x;
+
+    int occurences=0;
+
+    for(int ele:v){
+        if(ele==x){
+            occurences++;
+        }
+    }
+    cout<<occurences<<endl;
+
+    return 0;
+}

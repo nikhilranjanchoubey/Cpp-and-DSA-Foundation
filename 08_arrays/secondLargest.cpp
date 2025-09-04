fc1bb3977a56d6e82d2c7514da10975f916665bc
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-04 23:37:28 +0530

    array

--- 08_arrays/secondLargest.cpp
+++ 08_arrays/secondLargest.cpp
@@ -18,11 +18,18 @@ int largestElementIndex(int array[], int size){
 
 int main(){
 
-    int array[]={2,3,5,7,6,1};
-    int indexoflargest = largestElementIndex(array,6);
+    int array[]={2,3,5,7,6,1,7}; 
+    int n=7; // size of array
+    int indexoflargest = largestElementIndex(array,n);
 
-    array[indexoflargest]=-1;
-    int indexofsecondlargest = largestElementIndex(array,6);
+    //array[indexoflargest]=-1;
+    int largestelement = array[indexoflargest];
+    for (int i=0;i<n;i++){
+        if (array[i]==largestelement){
+            array[i]=-1;
+        }
+    }
+    int indexofsecondlargest = largestElementIndex(array,n);
     cout<<array[indexofsecondlargest]<<endl;
 
     return 0;

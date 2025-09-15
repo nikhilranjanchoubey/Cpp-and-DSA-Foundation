ff89d4cc1faba16956e301bf0d5191fa402e8fab
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-15 22:16:17 +0530

    problems

---
+++ 08_arrays/pairDifference.cpp
@@ -0,0 +1,38 @@
+/*Given a vector arr[] sorted in increasing order of n size and an integer x,
+find if there exists a pair in the array whose absolute difference is exactly x.*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5,10,15,20,26};
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]); // n -->> 5
+    int i = 0;
+    int j = n - 1;
+    bool found = false;
+
+    while (i < j) {
+        int diff = abs(arr[i] - arr[j]);
+        // What abs does:
+        //abs(-10) → 10  
+        //abs(10)  → 10  
+        //abs(0)   → 0
+        if (diff == x) {
+        
+            found = true;
+            break;
+        } else if (diff < x) {
+            i++; // try increasing the difference
+        } else {
+            j--; // try decreasing the difference
+        }
+    }
+
+    if (found == true)
+        cout<<"YES";
+
+    else cout <<"NO";
+
+    return 0;
+}

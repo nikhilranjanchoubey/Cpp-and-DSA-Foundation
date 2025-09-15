2f0919675b99addba7ee94d83a4235a46e4b1876
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-15 23:00:37 +0530

    problems

--- 08_arrays/sortedSquare.cpp
+++ 08_arrays/sortedSquare.cpp
@@ -5,20 +5,31 @@ Where size of vector 1<size<101.*/
 #include<iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int arr[] = {-4, -3, -1, 0, 2, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int arr[] = {0,1,2,3};
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]); // n -->> 4
-    int square[n];
+    int result[n];
+    int i = 0, j = n - 1, k = n - 1;
 
-    for(int i=0;i<n;i++){
-         square[i] = arr[i]*arr[i];
+    // Fill result[] from end to start
+    while (i <= j && k >= 0) {
+        if (abs(arr[i]) > abs(arr[j])) {
+            result[k] = arr[i] * arr[i];
+            i++;
+            k--;
+        } else {
+            result[k] = arr[j] * arr[j];
+            j--;
+            k--;
+        }
     }
-    for(int i=0;i<n;i++){
-       cout<< " " << square[i];
-    }
 
+    // Print the sorted squares
+    cout << "Sorted squares: ";
+    for (int i = 0; i < n; i++) {
+        cout << result[i] << " ";
+    }
 
     return 0;
 }

32396e0f263eca3db8fb87afdcd01cd1d1e990ba
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-21 19:34:09 +0530

    problems

--- 09_2D arrays/maximum1s.cpp
+++ 09_2D arrays/maximum1s.cpp
@@ -5,8 +5,31 @@
 #include<climits>
 using namespace std;
 
-int maximumOnesRow(vector<vector<int> >V){
+int leftMostOneRow(vector<vector<int>> &V) {
+    int leftMostOne = -1;
+    int maxOneRow = -1;
+    int j = V[0].size() - 1;
 
+    // Find leftmost 1 in the first row
+    while (j >= 0 && V[0][j] == 1) {
+        leftMostOne = j;
+        maxOneRow = 0;
+        j--;
+    }
+
+    // Check remaining rows
+    for (int i = 1; i < V.size(); i++) {
+        while (j >= 0 && V[i][j] == 1) {
+            leftMostOne = j;
+            maxOneRow = i;
+            j--;
+        }
+    }
+
+    return maxOneRow;
+}
+
+int maximumOnesRow(vector<vector<int> >V){
 
 int maxOnes = INT_MIN;
 int maxOnesRow = -1;
@@ -45,8 +68,13 @@ int main(){
         }
     }
 
-    int res = maximumOnesRow(vec);
-    cout << "Row with maximum 1s: " << res << endl;
+    int res1 = maximumOnesRow(vec);
+    cout << "Row with maximum 1s: " << res1 << endl;
+
+    int res2 = leftMostOneRow(vec);
+    cout << "Row with maximum 1s: " << res2 << endl;
 
     return 0;
 }
+
+

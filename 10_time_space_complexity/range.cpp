22e10ad8dc1817f794e9acef6a64c129fed42819
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-30 21:51:53 +0530

    Time Complexity

--- 10_time_space_complexity/range.cpp
+++ 10_time_space_complexity/range.cpp
@@ -12,7 +12,7 @@ int sum_in_range(int x,int y){
 int sum_in_range_optimised(int x, int y){
     int n = (y - x + 1);
     int a = x;
-    int result = (n * (2*a + (n-1)*1)) / 2;
+    int result = (n * (2 * a + (n - 1) * 1)) / 2;
     return result;
 }
 

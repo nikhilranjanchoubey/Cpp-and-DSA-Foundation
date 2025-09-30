9e1d4cfbcab6fb7c825f861a9c8f426e5819f32e
nikhilranjanchoubey <ranjannikhil476@gmail.com>
2025-09-30 21:55:51 +0530

    Time Complexity

--- 10_time_space_complexity/ques2.cpp
+++ 10_time_space_complexity/ques2.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hello World"; // 0(1)
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = 9;
+
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<"\n";
+    }
 
     return 0;
 }

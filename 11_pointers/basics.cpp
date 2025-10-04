431494a1a2ee3461858e7504a5d416e5c68c4644
nikhil ranjanchoubey <ranjannikhil476@gmail.com>
2025-10-04 18:09:12 +0530

    pointer

--- 11_pointers/basics.cpp
+++ 11_pointers/basics.cpp
@@ -3,10 +3,23 @@ using namespace std;
 
 int main(){
 
-    int n = 10;
+   int n = 10; // Declare an integer variable 'n' and initialize it with 10
 
-    cout<<"Value: "<<n<<endl; // value
-    cout<<"Address: "<<&n<<endl; // address
+    // Print the value stored in 'n'
+    cout << "Value: " << n << endl; // Output: Value: 10
 
-    return 0;
+    // Print the memory address of variable 'n'
+    cout << "Address: " << &n << endl; // Output: Address: (some hexadecimal address)
+
+    // Declare a pointer 'ptr' that stores the address of 'n'
+    int *ptr = &n;
+
+    // Print the value of the pointer (i.e., the address it holds)
+    cout << "Pointer: " << ptr << endl; // Output: Pointer: (same address as &n)
+
+    // Dereference the pointer to access the value stored at that address
+    cout << "Pointer: " << *ptr << endl; // Output: Pointer: 10
+
+    return 0; // Exit the program
 }
+

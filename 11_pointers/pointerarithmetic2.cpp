67fb90a12ac028f57d9dab1a6465e81c9bf5444a
nikhil ranjanchoubey <ranjannikhil476@gmail.com>
2025-10-05 18:25:10 +0530

    Pointer Arithmetic

---
+++ 11_pointers/pointerarithmetic2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x = 10;
+    int *ptr = &x;
+
+    cout<<ptr<<endl;
+
+    ptr += 1;
+
+    cout<<ptr<<endl;
+    
+    return 0;
+}

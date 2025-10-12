b6c074946a1f8a36c63f55401c0966cd4cdf40f6
nikhil ranjanchoubey <ranjannikhil476@gmail.com>
2025-10-12 17:39:52 +0530

    solution

--- Practice/16.cpp
+++ Practice/16.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-    int n = 5;
     
+    int n;
+
+    cout<<"Enter no. of rows: ";
+    cin>>n;    
 
     for (int i = 1; i <= n; i++) {
-
-        // Print increasing numbers
         for (int j = 1; j <= i; j++) {
             cout << j;
         }
-
         cout << endl;
     }
 

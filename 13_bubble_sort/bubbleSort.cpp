b352d1db79b0b6e79d0796b12dc901ee25a635d5
nikhil ranjanchoubey <ranjannikhil476@gmail.com>
2025-11-10 17:26:57 +0530

    bubble sort implementation

---
+++ 13_bubble_sort/bubbleSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Bubble Sort function to sort the vector in ascending order
+void bubbleSort(vector<int>& v) {
+    int n = v.size();
+
+    // Outer loop for each pass
+    for(int i = 0; i < n - 1; i++) {
+        // Inner loop for comparing adjacent elements
+        for(int j = 0; j < n - i - 1; j++) {
+            // Swap if elements are in wrong order
+            if(v[j] > v[j + 1]) {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    // Enter number of elements
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    // Declare vector of size n
+    vector<int> v(n);
+
+    // Input elements from user
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    // Call bubble sort
+    bubbleSort(v);
+
+    // Print sorted array
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}

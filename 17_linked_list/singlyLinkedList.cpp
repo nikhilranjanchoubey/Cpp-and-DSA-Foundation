82d23d970e67c7305bb68cd80bb23ac1586a3e05
nikhil ranjanchoubey <ranjannikhil476@gmail.com>
2025-12-15 22:41:06 +0530

    linked list

---
+++ 17_linked_list/singlyLinkedList.cpp
@@ -0,0 +1,27 @@
+/* Implementation of a listnode in a singly linked list. */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+int main()
+{
+
+    Node *n = new Node(1);
+    cout << n->val << " " << n->next << endl;
+
+    return 0;
+}

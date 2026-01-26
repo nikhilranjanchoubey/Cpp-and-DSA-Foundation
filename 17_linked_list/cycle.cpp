c43dfdd4aa7ee440b3a806c3194cf512f55ced4f
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-01-26 19:42:21 +0530

    Floyd’s Cycle Detection Algorithm

---
+++ 17_linked_list/cycle.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertAtTail(int value)
+    {
+        Node *new_node = new Node(value);
+        if (head == NULL)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+// Floyd’s Cycle Detection Algorithm
+bool detectCycle(Node *head)
+{
+    if (!head)
+        return false;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    LinkedList ll1;
+    ll1.insertAtTail(1);
+    ll1.insertAtTail(2);
+    ll1.insertAtTail(3);
+    ll1.insertAtTail(4);
+    ll1.insertAtTail(5);
+    ll1.insertAtTail(6);
+    ll1.insertAtTail(7);
+    ll1.insertAtTail(8);
+
+    ll1.display();
+
+    // Creating a cycle:
+    // last node points to node with value 4
+    ll1.head->next->next->next->next->next->next->next->next =
+        ll1.head->next->next->next;
+
+    cout << detectCycle(ll1.head) << endl;
+
+    return 0;
+}

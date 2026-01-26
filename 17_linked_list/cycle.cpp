d823f0e40879398a51ace39b4809d61545589f05
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-01-26 22:21:00 +0530

    Floyd’s Cycle Detection Algorithm

--- 17_linked_list/cycle.cpp
+++ 17_linked_list/cycle.cpp
@@ -68,32 +68,71 @@ bool detectCycle(Node *head)
         fast = fast->next->next;
 
         if (slow == fast)
+        {
+            cout << "Cycle detected at node with value: " << slow->val << endl;
+
             return true;
+        }
     }
 
     return false;
 }
+// Extra
+void removeCycle(Node *&head)
+{
+    // assuming that the linked llist has a cycle
+    Node *slow = head;
+    Node *fast = head;
+
+    do
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    } while (slow != fast);
+
+    fast = head;
+    while (slow->next != fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    slow->next = NULL;
+}
 
 int main()
 {
-    LinkedList ll1;
-    ll1.insertAtTail(1);
-    ll1.insertAtTail(2);
-    ll1.insertAtTail(3);
-    ll1.insertAtTail(4);
-    ll1.insertAtTail(5);
-    ll1.insertAtTail(6);
-    ll1.insertAtTail(7);
-    ll1.insertAtTail(8);
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.insertAtTail(7);
+    ll.insertAtTail(8);
 
-    ll1.display();
+    // ll1.display();
 
     // Creating a cycle:
-    // last node points to node with value 4
-    ll1.head->next->next->next->next->next->next->next->next =
-        ll1.head->next->next->next;
+    ll.head->next->next->next->next->next->next->next->next =
+        ll.head->next->next;
 
-    cout << detectCycle(ll1.head) << endl;
+    cout << "\nChecking if cycle exists in the linked list..." << endl;
+    cout << "Cycle Status: "
+         << (detectCycle(ll.head) ? "Present" : "Not Present") << endl;
+
+    removeCycle(ll.head);
+
+    cout << "Removing cycle from the linked list..." << endl;
+    cout << "Cycle Status After Removal: "
+         << (detectCycle(ll.head) ? "Present" : "Not Present") << endl;
+
+    cout << "Final Linked List (After Cycle Removal):" << endl;
+
+    ll.display();
+
+    ;
 
     return 0;
 }

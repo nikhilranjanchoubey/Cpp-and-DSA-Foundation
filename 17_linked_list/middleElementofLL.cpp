72717aeda500839374738a68c2d4a76f08012213
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-01-26 19:25:18 +0530

    Slow fast pointer

---
+++ 17_linked_list/middleElementofLL.cpp
@@ -0,0 +1,87 @@
+/* Pattern: Slow fast pointer
+Find the middle element of the given linked list.*/
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertAtTail(int value)
+    {
+        Node *new_node = new Node(value);
+        if (head == NULL)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+Node *findMiddleElement(Node *&head)
+{
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast !=NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main()
+{
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.display();
+
+    Node *middleNode = findMiddleElement(ll.head);
+    cout << middleNode->val << endl;
+
+    return 0;
+}

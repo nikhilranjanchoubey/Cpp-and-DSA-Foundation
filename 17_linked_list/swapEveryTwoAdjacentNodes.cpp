16549a1cea2e232526bb0104436ee63ff9e5bd2f
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-01-27 19:09:30 +0530

    swap every two adjacent nodes

---
+++ 17_linked_list/swapEveryTwoAdjacentNodes.cpp
@@ -0,0 +1,95 @@
+/* Given a linked list, swap every two adjacent nodes and return its head.
+You may not modify the valuesin the lists nodes.
+Only nodes themselves may be changed. */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertAtTail(int value)
+    {
+        Node *new_node = new Node(value);
+        if (head == NULL)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+Node *swapAdjacent(Node *&head)
+{
+
+    // base case
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    // recursive case
+    Node *secondNode = head->next;
+    head->next = swapAdjacent(secondNode->next);
+    secondNode->next = head; // reversing the link between first and second node
+
+    return secondNode;
+}
+
+int main()
+{
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+
+    cout << "\nGiven Linked List: " << endl;
+    ll.display();
+
+    ll.head = swapAdjacent(ll.head);
+    cout << "\nNew Linked List" << endl;
+    ll.display();
+
+    return 0;
+}

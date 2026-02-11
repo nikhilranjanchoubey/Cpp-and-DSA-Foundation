3cc48a7ce6a064ce37b49b4f18a67477c12d253e
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-02-11 22:17:34 +0530

    circular linked list

---
+++ 19_circular_linked_list/circularLinkedList.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class CircularLinkedList
+{
+public:
+    Node *head;
+
+    CircularLinkedList()
+    {
+        head = NULL;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        do
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        while (temp != head);
+        cout<<endl;
+    }
+
+    void printCircular(){
+        Node* temp=head;
+        for(int i = 0;i<15;i++){
+            cout<<temp->val<<"->";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+
+    void insertAtStart(int val)
+    {
+        Node *new_node = new Node(val);
+        if (head == NULL)
+        {
+            head = new_node;
+            new_node->next = head; // circular linked list
+            return;
+        }
+
+        Node *tail = head;
+        while (tail->next != head)
+        {
+            tail = tail->next;
+        }
+        // now tail is pointing to the last node
+        tail->next = new_node;
+        new_node->next = head;
+        head = new_node;
+    }
+};
+
+int main(){
+
+    CircularLinkedList cll;
+    cll.insertAtStart(3);
+    cll.insertAtStart(2);
+    cll.insertAtStart(1);
+    cll.display();
+    cll.printCircular();
+
+
+    return 0;
+}

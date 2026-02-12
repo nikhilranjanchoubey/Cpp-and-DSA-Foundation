b963c849d6f2db951806786aab1e271298e3de25
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-02-12 21:41:00 +0530

    circular linked list

--- 19_circular_linked_list/circularLinkedList.cpp
+++ 19_circular_linked_list/circularLinkedList.cpp
@@ -31,18 +31,19 @@ public:
         {
             cout << temp->val << "->";
             temp = temp->next;
-        }
-        while (temp != head);
-        cout<<endl;
+        } while (temp != head);
+        cout << endl;
     }
 
-    void printCircular(){
-        Node* temp=head;
-        for(int i = 0;i<15;i++){
-            cout<<temp->val<<"->";
-            temp=temp->next;
+    void printCircular()
+    {
+        Node *temp = head;
+        for (int i = 0; i < 15; i++)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
         }
-        cout<<endl;
+        cout << endl;
     }
 
     void insertAtStart(int val)
@@ -65,17 +66,84 @@ public:
         new_node->next = head;
         head = new_node;
     }
+    void insertAtEnd(int val)
+    {
+
+        Node *new_node = new Node(val);
+
+        if (head == NULL)
+        {
+            head = new_node;
+            new_node->next = head; // circular linked list
+            return;
+        }
+
+        Node *tail = head;
+        while (tail->next != head)
+        {
+            tail = tail->next;
+        }
+        // here tail will be pointing to last node
+
+        tail->next = new_node;
+        new_node->next = head;
+    }
+    void deleteAtStart()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        Node *temp = head;
+        Node *tail = head;
+        while (tail->next != head)
+        {
+            tail = tail->next;
+        }
+
+        head = head->next;
+        tail->next = head;
+        free(temp);
+    }
+    void deleteAtEnd()
+    {
+
+        if (head == NULL)
+        {
+            return;
+        }
+
+        Node *tail = head;
+        while (tail->next->next != head)
+        {
+            tail = tail->next;
+        }
+        // here tail is pointing to the second last node
+
+        Node *temp = tail->next; // to be deleted
+        tail->next = head;
+        free(temp);
+    }
 };
 
-int main(){
+int main()
+{
 
     CircularLinkedList cll;
     cll.insertAtStart(3);
     cll.insertAtStart(2);
     cll.insertAtStart(1);
     cll.display();
-    cll.printCircular();
+    // cll.printCircular();
 
+    cll.insertAtEnd(8);
+    cll.display();
+
+    cll.deleteAtStart();
+    cll.display();
+
+    cll.deleteAtEnd();
+    cll.display();
 
     return 0;
 }

4b2b9a5b5e618570b443484bbc55e56baed23843
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-02-17 23:39:28 +0530

    bracket sequence is balanced or not

---
+++ 20_stacks/balancedBracketSequence.cpp
@@ -0,0 +1,45 @@
+/* Check whether a given bracket sequence is balanced or not? */
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string str)
+{
+    stack<char> st;
+    for (int i = 0; i < str.size(); i++)
+    {
+        char ch = str[i]; // current char
+        if (ch == '[' or ch == '{' or ch == '(')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            // closing bracket
+            if (ch == ')' and !st.empty() and st.top() == '(')
+            {
+                st.pop();
+            }
+            else if (ch == ']' and !st.empty() and st.top() == '[')
+            {
+                st.pop();
+            }
+            else if (ch == '}' and !st.empty() and st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+int main()
+{
+    string str = "[({})()[](({}))]";
+    cout << isValid(str) << endl;
+    return 0;
+}

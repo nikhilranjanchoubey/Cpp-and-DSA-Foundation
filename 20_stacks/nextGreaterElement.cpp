e99dfd7b08b8cc293d751cb69a343240798d3ed9
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-02-20 23:34:38 +0530

    next greater elements

---
+++ 20_stacks/nextGreaterElement.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> nge(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> output(n, -1);
+    stack<int> st;
+    st.push(0);
+    for (int i = 1; i < n; i++)
+    {
+        while (!st.empty() and arr[i] > arr[st.top()])
+        {
+            output[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    while (not st.empty())
+    { // not mandatory while loop
+        output[st.top()] = -1;
+        st.pop();
+    }
+
+    return output;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter total no. of elements: ";
+    cin >> n;
+    vector<int> v;
+    cout << "Enter Elements: " << endl;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    cout << "Next Greater Elements: " << endl;
+    vector<int> res = nge(v);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+
+    return 0;
+}

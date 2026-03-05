19af58651d1164f5503d7d08c6d3c410784ab620
Nikhil Ranjan Choubey <ranjannikhil476@gmail.com>
2026-03-05 14:56:07 +0530

    hashing

--- 23_hashmap/hashTableClosedAddressing.cpp
+++ 23_hashmap/hashTableClosedAddressing.cpp
@@ -6,52 +6,62 @@
 #include <algorithm>
 using namespace std;
 
-class Hashing {
+class Hashing
+{
 
     vector<list<int>> hashtable;
     int buckets;
 
 public:
-
-    Hashing(int size){
+    Hashing(int size)
+    {
         buckets = size;
         hashtable.resize(size);
     }
 
-    int hashvalue(int key){
-        return key % buckets;   // division method for hash function
+    int hashvalue(int key)
+    {
+        return key % buckets; // division method for hash function
     }
 
-    void add(int key){
+    void add(int key)
+    {
         int idx = hashvalue(key);
         hashtable[idx].push_back(key);
     }
 
-    list<int>::iterator searchke(int key){
+    list<int>::iterator searchke(int key)
+    {
         int idx = hashvalue(key);
         return find(hashtable[idx].begin(), hashtable[idx].end(), key);
     }
 
-    void deletekey(int key){
+    void deletekey(int key)
+    {
 
         int idx = hashvalue(key);
 
         auto it = searchke(key);
 
-        if(it != hashtable[idx].end()){   // key is present
+        if (it != hashtable[idx].end())
+        { // key is present
             hashtable[idx].erase(it);
-            cout<<endl;
+            cout << endl;
             cout << "Key deleted successfully\n";
         }
-        else{
+        else
+        {
             cout << "Key not found\n";
         }
     }
 
-    void display(){
-        for(int i = 0; i < buckets; i++){
+    void display()
+    {
+        for (int i = 0; i < buckets; i++)
+        {
             cout << i << " -> ";
-            for(auto x : hashtable[i]){
+            for (auto x : hashtable[i])
+            {
                 cout << x << " ";
             }
             cout << endl;
@@ -59,7 +69,8 @@ public:
     }
 };
 
-int main(){
+int main()
+{
 
     Hashing h(7);
 
